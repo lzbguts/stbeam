6c28b5337e2a7b4919040d3078aa7a8f7d597c7f
gutsfer <guts.felicidade@gmail.com>
2023-02-08 21:13:32 -0300

    added ads

--- src/views/GameView/GameView.vue
+++ src/views/GameView/GameView.vue
@@ -1,25 +1,35 @@
 <template>
     <div class="container-main">
-        <div v-show="!isLoading" class="game-info">
-            <a :href="url_game"><img :src="img" :alt="nome"></a>
-            <h1>{{ nome }}</h1>
-            <GameContent @recursos="carregarRecursos"/>
+        <div>
+            <Ads/>
         </div>
-        <div v-show="isLoading">
-            <SplashScreen/>
+        <div>
+            <div v-show="!isLoading" class="game-info">
+                <a :href="url_game"><img :src="img" :alt="nome"></a>
+                <h1>{{ nome }}</h1>
+                <GameContent @recursos="carregarRecursos" />
+            </div>
+            <div v-show="isLoading">
+                <SplashScreen />
+            </div>
+        </div>
+        <div>
+            <Ads/>
         </div>
     </div>
 </template>
 
 <script>
 import GameContent from '../../components/GameView/GameContent/GameContent.vue';
-import SplashScreen from '../../components/SplashScreen/SplashScreen.vue'
+import SplashScreen from '../../components/SplashScreen/SplashScreen.vue';
+import Ads from '../../components/Ads/Ads.vue';
 
 export default {
     name: "GameView",
     components: {
         GameContent,
-        SplashScreen
+        SplashScreen,
+        Ads
     },
     data() {
         return {
@@ -30,7 +40,7 @@ export default {
         }
     },
     methods: {
-        carregarRecursos({nome, img, id}) {
+        carregarRecursos({ nome, img, id }) {
             this.url_game = `https://store.steampowered.com/app/${id}`;
             this.nome = nome;
             this.img = img;
@@ -41,4 +51,6 @@ export default {
     }
 }
 </script>
-<style scoped src="./GameView.css"></style>
+<style scoped src="./GameView.css">
+
+</style>
