25e6c9558a1a19b4bc19bc83d54d05ab1acef8e1
gutsfer <guts.felicidade@gmail.com>
2023-02-08 21:40:47 -0300

    added ads fix 4

--- src/components/Ads/SideAd.vue
+++ src/components/Ads/SideAd.vue
@@ -1,14 +1,6 @@
 <template>
     <div>
-        <component is="script" async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7475443984984626"
-            crossorigin="anonymous"></component>
-        <!-- Games - Side -->
-        <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7475443984984626"
-            data-ad-slot="4413824474" data-ad-format="auto" data-full-width-responsive="true"></ins>
-        <component is="script">
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </component>
+        
     </div>
 </template>
 
